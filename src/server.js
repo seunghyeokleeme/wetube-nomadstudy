aa951bcee912ff971a9ae18e46ef180dc843cd5f
devinan <devinan.hr@gmail.com>
2022-08-09 22:43:38 +0900

    req, res object

--- src/server.js
+++ src/server.js
@@ -4,10 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Somebody is trying to go home.");
+const handleHome = (req, res) => res.send("I still love you.");
+const handleLogin = (req, res) => res.send("Login here.");
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 app.listen(PORT, () =>
-  console.log(`Server listening on port http://localhost:${PORT}`)
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
 );
