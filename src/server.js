611e0490d0c74b2847ead3056642ed86c950df90
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-11 00:45:35 +0900

    Add logger middleware in server.js

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => res.send("I still love you.");
-const handleLogin = (req, res) => res.send("Login here.");
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", logger, handleHome);
 
 app.listen(PORT, () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
