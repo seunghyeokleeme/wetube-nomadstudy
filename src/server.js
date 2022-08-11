b50892e5c6152a7fe9a4d3ddca177111f2c9c135
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-11 21:46:38 +0900

    Add morgan in server.js

--- src/server.js
+++ src/server.js
@@ -1,21 +1,15 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const routerLogger = (req, res, next) => {
-  console.log(req.path);
-  next();
-};
-const methodLogger = (req, res, next) => {
-  console.log(req.method);
-  next();
-};
 const home = (req, res) => res.send("<h1>Hello</h1>");
 const login = (req, res) => res.send("<h1>Login</h1>");
 
-app.use(methodLogger, routerLogger);
+app.use(logger);
 app.get("/", home);
 app.get("/login", login);
 
