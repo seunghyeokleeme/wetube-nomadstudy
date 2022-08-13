a7ed05d5e3c8432d46e4c8252456bef4704c5320
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-14 01:05:47 +0900

    Add Router

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => res.send("<h1>Hello</h1>");
-const login = (req, res) => res.send("<h1>Login</h1>");
-
 app.use(logger);
-app.get("/", home);
-app.get("/login", login);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 app.listen(PORT, () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`)
