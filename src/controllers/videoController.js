4c69287cc4cf8db60b47aa7929a22c8704705126
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-15 02:12:47 +0900

    Pug Mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,37 @@
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", potato: "tomato" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
-export const search = (req, res) => res.send("search");
-export const deleteVideo = (req, res) => {
-  return res.send(`Remove Video #${req.params.id}`);
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "1 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "1 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "1 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
 };
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
+export const search = (req, res) => res.send("search");
 export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => {
+  return res.send("Remove Video");
+};
 export const comments = (req, res) => res.send("comments");
