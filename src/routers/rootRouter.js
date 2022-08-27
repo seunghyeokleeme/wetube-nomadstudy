8242303c36b5046c5b7aae917efa4799514c7bcf
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-27 23:36:41 +0900

    logout done:)

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -2,7 +2,6 @@ import express from "express";
 import {
   getJoin,
   getLogin,
-  logout,
   postJoin,
   postLogin,
 } from "../controllers/userController";
@@ -13,7 +12,6 @@ const rootRouter = express.Router();
 rootRouter.get("/", home);
 rootRouter.route("/join").get(getJoin).post(postJoin);
 rootRouter.route("/login").get(getLogin).post(postLogin);
-rootRouter.get("/logout", logout);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,15 +2,15 @@ import express from "express";
 import {
   edit,
   finishGithubLogin,
-  remove,
+  logout,
   see,
   startGithubLogin,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
