1ea00615fc073a9f9cd834a9f60a63066eb2ed3e
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-28 04:24:22 +0900

    8.7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -141,9 +141,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id, email: sessionEmail, username: sessionUsername },
+      user: { _id, avatarUrl, email: sessionEmail, username: sessionUsername },
     },
     body: { name, email, username, location },
+    file,
   } = req;
   let searchParam = [];
   if (sessionEmail !== email) {
@@ -161,9 +162,11 @@ export const postEdit = async (req, res) => {
       });
     }
   }
+
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
