09ef43e429e019c322cfcf7aa2b94be5ce3ef799
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-28 23:03:37 +0900

    BugFix

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -102,7 +102,6 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
-    console.log(userData);
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
@@ -223,7 +222,6 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id).populate("videos");
-  console.log(user);
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found" });
   }
