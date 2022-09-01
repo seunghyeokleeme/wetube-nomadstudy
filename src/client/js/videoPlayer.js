c0384aa8a44ab26c44f666008b76d216b9d6b5e3
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-09-01 13:51:11 +0900

    Current & Duration Time

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,9 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -38,6 +39,19 @@ const handleVolumeChange = event => {
   video.volume = value;
 };
 
+const formatTime = seconds =>
+  new Date(seconds * 1000).toISOString().substring(14, 19);
+
+const handleLoadedMetadata = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+  currenTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
